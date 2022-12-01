ad2e2cbefddb31116065d2d14d88e338826c0b3a
Sylvain <sylvainriondet@gmail.com>
2022-12-01 15:45:51 +0100

    moved to src, using pkgutil, fixing row writing

--- src/populate_file_names_to_main_csv.py
+++ src/populate_file_names_to_main_csv.py
@@ -2,42 +2,43 @@
     the filenames and the column headers
 """
 import csv
-import sys
+import pkgutil
 from argparse import ArgumentParser
 from datetime import datetime as dt
 from pathlib import Path
 import filetype
 
 DEFAULT_MAIN_CSV = f"{dt.now():%Y-%m-%d_%H-%M}_descriptor.csv"
-CSV_TEMPLATE = "template.csv"
+PATH_CSV_TEMPLATE = "../CSV_Samples/template.csv"
 
 
 def main(folder: Path, path_output: Path):
     """ Scan a folder and add filenames to a CSV with column headers """
-    # Fetch the template CSV
-    path_csv_template = Path(sys.path[0]) / CSV_TEMPLATE
-
     # Check paths
-    assert path_csv_template.is_file(), FileNotFoundError(f"Couldn't find the CSV template at {path_csv_template}")
+    # assert PATH_CSV_TEMPLATE.is_file(), FileNotFoundError(f"Couldn't find the CSV template at {PATH_CSV_TEMPLATE}")
     assert folder.is_dir(), NotADirectoryError(f"Input folder doesn't exist or is not a directory: {folder}")
     path_output.parent.mkdir(exist_ok=True)
 
     # Fetch the file names
-    media_files = (file for file in folder.iterdir()
+    media_files = (str(file.name).strip() for file in folder.iterdir()
                    if file.is_file() and (filetype.is_image(file) or filetype.is_video(file)))
+
+    # Get the template saved in project
+    bin_csv_template = pkgutil.get_data(__package__, PATH_CSV_TEMPLATE)
+    template_io = bin_csv_template.decode("utf-8")
+    headers = template_io.split(",")
 
     # Create the CSV
     n = 0
-    with open(path_csv_template, "r") as csv_template, open(path_output, "w") as out_csv:
-        template_reader = csv.reader(csv_template)
+    with open(path_output, "w") as out_csv:
         output_writer = csv.writer(out_csv)
 
         # Write the default column headers
-        output_writer.writerow(next(template_reader))
+        output_writer.writerow(headers)
 
         # Write filename in each line
         for filename in media_files:
-            output_writer.writerow(str(filename))
+            output_writer.writerow((filename, ))
             n += 1
 
     print(f"Done. File with {n} filenames written at: {path_output}")
