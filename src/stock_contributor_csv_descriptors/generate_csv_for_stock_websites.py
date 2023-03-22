da641a4f94495edbdb5d8e50d99551dd76fa5bb1
Sylvain <sylvainriondet@gmail.com>
2023-03-22 23:12:17 +0100

    skeleton of the steps to create the specific csvs

--- src/stock_contributor_csv_descriptors/generate_csv_for_stock_websites.py
+++ src/stock_contributor_csv_descriptors/generate_csv_for_stock_websites.py
@@ -1,6 +1,6 @@
 """
-From a media descriptor CSV, filled by the user, generate a CSV specific to each contributor platform.
-That media descriptor can be created by the other script, populate_file_names_to_main_csv.py
+From a master media descriptor CSV, filled by the user, generate a specific CSV to each contributor platform.
+That master file can be created by the other script, populate_file_names_to_main_csv.py
 """
 
 from argparse import ArgumentParser
@@ -9,21 +9,41 @@ from pathlib import Path
 from src.stock_contributor_csv_descriptors.common import is_dir_path, DEFAULT_MAIN_CSV, PATH_CSV_TEMPLATE, is_file_path
 
 
-def main(descriptor_path: Path, output_dir: Path):
+# Constants
+PLATFORMS = ("adobe", "pond5", "shutterstock")
+PLATFORMS_DEFAULT = ",".join(PLATFORMS)
+
+
+def main(descriptor_path: Path, output_dir: Path, platforms: tuple[str]):
+    # Load the master media descriptor
+
+    # Load the Template and Format descriptor
+
+    # Check the values of the Master csv from by user with the Format descriptor
+
+    # Use the Platform Specific templates to generate each file
+    for platform in platforms:
+        pass
     pass
 
 
 def cli():
     """ Parse the arguments """
     parser = ArgumentParser(__doc__)
-    parser.add_argument("descriptor", help="General media descriptor file (default: %(default)s)",
+    parser.add_argument("master", help="Master media descriptor file (default: %(default)s)",
                         default=Path.cwd() / DEFAULT_MAIN_CSV, type=is_file_path)
     parser.add_argument("-o", "--output", help="Base directory for the specific descriptors (default: %(default)s)",
                         default=Path.cwd(), type=is_dir_path)
+    parser.add_argument("-p", "--platforms", help="Target platforms, comma separated (default: %(default)s)",
+                        default=PLATFORMS_DEFAULT, type=str)
+    # todo: use "choices" with multiple values for the ArgParse?
 
     args = parser.parse_args()
-    main(Path(args.descriptor), Path(args.output))
+    platforms = args.platforms.split(",")
+
+    main(Path(args.master), Path(args.output), args.platforms)
 
 
 if __name__ == '__main__':
     cli()
+
